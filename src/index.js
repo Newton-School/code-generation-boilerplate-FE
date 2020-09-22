151c8c19b1ec8f3e05f5f9cf63b239fe5584a4d3
GaryGosh <clichethe1@yahoo.com>
2020-09-23 01:36:57 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 function generateCodeFromObject(obj){
     //return a code generated string
+    return (
+        <>
+            <Clock style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+                <Hour />
+                <Minute style={{ color: "green", fontSize: 30 }} />
+            </Clock>
+        </>
+    )
    }
    
    module.exports=generateCodeFromObject;
