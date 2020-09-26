554a44ad46bedd73649ddd3726b92aebc0fcefe3
ajay singh <you@example.com>
2020-09-26 00:04:40 -0700

    jsx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-   module.exports=generateCodeFromObject;
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+
+const state = {
+	type: "div",
+	name: "Clock",
+	root: true,
+	style: {
+		display: "flex",
+		"flex-direction": "row",
+		"justify-content": "center",
+	},
+	children: [
+		{
+			type: "div",
+			name: "Hour",
+			style: {},
+			children: [],
+		},
+		{
+			type: "span",
+			name: "Minute",
+			style: {
+				color: "green",
+				"font-size": 30,
+			},
+			children: [],
+		},
+	],
+};
+
+console.log(state.style);
+
+const Component1 = React.createElement(
+	state.type,
+	{ style: state.style },
+	state.name,
+	state.children.map((child, index) =>
+		React.createElement(child.type, { style: child.style }, child.name)
+	)
+);
+
+ReactDOM.render(Component1, document.getElementById("root"));
+
+serviceWorker.unregister();
